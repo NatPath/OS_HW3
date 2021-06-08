b26672d659479621814f8ad68f8f803f20ca49bc
BenHalpe <benhalpe@gmail.com>
2021-06-08 17:12:01 +0300

    added condition variables to queue

--- webserver-files/queue.c
+++ webserver-files/queue.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <queue.h>
+#include <pthread.h>
 typedef struct node{
    int _value;
    struct node * _next;
@@ -16,8 +17,10 @@ Node nodeCreate(int value){
 typedef struct queue{
     Node _head;    
     Node _tail;
-    int _size;
+    pthread_cond_t _cond;
     int _max_capacity;
+    pthread_mutex_t _mutex;
+    int _size;
 }* Queue;
 
 Queue queueCreate(int max_capacity){
@@ -25,12 +28,20 @@ Queue queueCreate(int max_capacity){
     q->_head=NULL;
     q->_tail=NULL;
     q->_size=0;
+    pthread_cond_init(&q->_cond,NULL);
+    pthread_mutex_init(&q->_mutex,NULL);
     q->_max_capacity=max_capacity;
     return q;
 }
  void enQueue(Queue q,int new_val){
-     Node new_node = nodeCreate(new_val);
-     if (q->_size==0){
+    
+     
+     /*
+     
+     */
+    pthread_mutex_lock(&q->_mutex);
+    Node new_node = nodeCreate(new_val);
+    if (q->_size==0){
          q->_head = new_node;
          q->_tail = new_node;
      }
@@ -38,7 +49,11 @@ Queue queueCreate(int max_capacity){
          q->_tail->_next = new_node;
          q->_tail = q->_tail->_next;
      }
-     q->_size++;
+    q->_size++;
+    pthread_cond_signal(&q->_cond);
+    pthread_mutex_unlock(&q->_mutex);
+
+
  }
  int topQueue(Queue q){
      if (q->_size != 0){
@@ -49,16 +64,19 @@ Queue queueCreate(int max_capacity){
      }
  }
  int deQueue(Queue q){
-     int res;
-     if (q->_size != 0){
-         res = topQueue(q);
-         Node temp = q->_head;
-         q->_head = q->_head->_next; 
-         free(temp);
-         q->_size--;
-         return res;
-     }
-     else{
-         return -1; // the queue is empty
-     }
+
+    int res;
+    pthread_mutex_lock(&q->_mutex);
+    while(q->_size==0){
+        pthread_cond_wait(&q->_cond,&q->_mutex);
+    }
+    res = topQueue(q);
+    Node temp = q->_head;
+    q->_head = q->_head->_next; 
+    free(temp);
+    q->_size--;
+    pthread_mutex_unlock(&q->_mutex);
+    return res;
+     
+    
  }
